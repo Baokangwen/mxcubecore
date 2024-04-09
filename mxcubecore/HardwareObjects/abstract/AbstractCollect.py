b5325d11568bdc5a3cebf754791948f6dbb08cf6
DESKTOP-LHUGQ4C\DQ <563451099@qq.com>
2024-04-09 20:35:53 +0800

    dev

--- mxcubecore/HardwareObjects/abstract/AbstractCollect.py
+++ mxcubecore/HardwareObjects/abstract/AbstractCollect.py
@@ -616,6 +616,9 @@ class AbstractCollect(HardwareObject, object):
             params["slitGapVertical"] = vert_gap
             try:
                 collection_id = HWR.beamline.lims.update_data_collection(params)
+                logging.getLogger("HWR").info(
+                    "==== Update data collection in LIMS, collection_id: %s" % collection_id
+                )
                 if collection_id:
                     self.current_dc_parameters["collection_id"] = collection_id
             except BaseException:
